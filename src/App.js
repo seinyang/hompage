bf1fcc06a0c22b77cc8fbf4151fdb2d1e15d9d77
seinyang <did756984@naver.com>
2022-10-12 18:25:23 +0900

    aa

--- src/App.js
+++ src/App.js
@@ -3,48 +3,117 @@
 
 import Header from "./component/header";
 import Slider from "./component/slider";
-import Defaultsection from"./component/defaultsection";
-import Defaultsection2 from"./component/defaultsection2";
-import News1 from"./component/news1";
+import Defaultsection from "./component/defaultsection";
+import Defaultsection2 from "./component/defaultsection2";
+import News1 from "./component/news1";
 import Download from "./component/download";
 import Footer from ".//component/footer";
-import {BrowserRouter,Link,Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
+//import Home from "./component/ss";
 
 // div를 묶어서 return해주기때문에
-import {Fragment} from "react";
-
-
-
+import { Fragment } from "react";
 
 function App() {
   return (
-   
     <Fragment>
-<BrowserRouter>
-      <Route>
-    <Header />
-    
-      <Route path="./nexmore/about.html" element={}></Route>
-      <Route path="nexmore/ceo.html" element={<Header />}>CEO인사말</Route>
-      <Route path=".nexmore/history.html" element={<Header />}>회사연혁</Route>
-      
-    <Slider />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route
+            path="/Header"
+            element={
+              <div>
+                <Link to="#">ai&big data</Link>
 
-    <Defaultsection />
+                <ul>
+                  <li>
+                    <Link to="nexmore/about.html">넥스모어소개</Link>
+                  </li>
+                </ul>
+              </div>
+            }
+          />
+          <Route
+            path="/Header"
+            element={
+              <div>
+                <li>
+                  <Link to="nexmore/ceo.html">CEO인사말</Link>
+                </li>
+              </div>
+            }
+          />
 
-    <Defaultsection2 />
+          <Route
+            path="/Header"
+            element={
+              <div>
+                <li>
+                  <Link to="nexmore/history.html">회사연혁</Link>
+                </li>
+              </div>
+            }
+          />
 
-    <News1 />
+          <Route
+            path="/Header"
+            element={
+              <div>
+                <li>
+                  <Link to="nexmore/org.html">조직도</Link>
+                </li>
+              </div>
+            }
+          />
 
-    <Download />
-    
-    <Footer />
-    </Route>
-</BrowserRouter>
+          <Route
+            path="/Header"
+            element={
+              <div>
+                <li>
+                  <Link to="nexmore/partner.html">파트너</Link>
+                </li>
+              </div>
+            }
+          />
+
+          <Route
+            path="/Header"
+            element={
+              <div>
+                <li>
+                  <Link to="nexmore/recruit.html">인재채용</Link>
+                </li>
+              </div>
+            }
+          />
+
+          <Route
+            path="/Header"
+            element={
+              <div>
+                <li>
+                  <Link to="nexmore/map.html">오시는길</Link>
+                </li>
+              </div>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+      <Slider />
+
+      <Defaultsection />
+
+      <Defaultsection2 />
+
+      <News1 />
+
+      <Download />
+
+      <Footer />
     </Fragment>
-    
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,30 +3,27 @@
 //  @version :0.1                                             //
 //  @Note    :  리엑트 기본 설정                               //
 //                                                            //
-//  @Date    : 2022-10-09                                     // 
+//  @Date    : 2022-10-09                                     //
 ////////////////////////////////////////////////////////////////
 
-
-
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
 //index.js는 메인프로그램
 //index.html과 index.js는 같이 움직이는것.root해놓은것에 삽입됨
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
-    {/* app.js는 불려와서 여기 렌더링됨 */}
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      {/* app.js는 불려와서 여기 렌더링됨 */}
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
