280f394fe6624d833982f9b02f95a177931cc4ec
sein yang <did756984@naver.com>
2022-10-21 00:07:29 +0900

    one

--- src/component/mainpage.js
+++ src/component/mainpage.js
@@ -10,6 +10,9 @@ import { Link } from "react-router-dom";
 
 function Mainpage() {
   return (
+
+ 
+ 
     <div>
       {/* slider.js */}
       <section
@@ -17,191 +20,81 @@ function Mainpage() {
         data-start-height="700"
         data-slide-overlay="yes"
       >
-        <div class="tp-banner-container">
-          <div class="tp-banner">
-            <ul>
-              <li
-                data-transition="fade"
-                data-slotamount="1"
-                data-masterspeed="1000"
-                data-thumb="images/main-slider/1.jpg"
-                data-saveperformance="off"
-                data-title="Awesome Title Here"
-              >
-                <img
-                  src="images/main-slider/1.jpg"
-                  alt=""
-                  data-bgposition="center top"
-                  data-bgfit="cover"
-                  data-bgrepeat="no-repeat"
-                />
-
-                <div class="slide-overlay"></div>
-                <div
-                  class="tp-caption sft sfb tp-resizeme"
-                  data-x="left"
-                  data-hoffset="15"
-                  data-y="center"
-                  data-voffset="-90"
-                  data-speed="1500"
-                  data-start="0"
-                  data-easing="easeOutExpo"
-                  data-splitin="none"
-                  data-splitout="none"
-                  data-elementdelay="0.01"
-                  data-endelementdelay="0.3"
-                  data-endspeed="1200"
-                  data-endeasing="Power4.easeIn"
-                >
-                  <div class="text-left">
-                    <h2>IT의 가치 넥스모어가 만들어 갑니다.</h2>
-                  </div>
+ 
+ <div id="categoryExampleSlider" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#categoryExampleSlider" data-slide-to="0" class="active"></li>
+                    <li data-target="#categoryExampleSlider" data-slide-to="1"></li>
+                   
+                </ol>
+                <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                        <img src="images/main-slider/1.jpg"   />
+                        <div class="carousel-caption">
+                         <div
+                            class="tp-caption sft sfb tp-resizeme"
+                            data-x="left"
+                            data-hoffset="15"
+                            data-y="center"
+                            data-voffset="-90"
+                            data-speed="1500"
+                            data-start="0"
+                            data-easing="easeOutExpo"
+                            data-splitin="none"
+                            data-splitout="none"
+                            data-elementdelay="0.01"
+                            data-endelementdelay="0.3"
+                            data-endspeed="1200"
+                            data-endeasing="Power4.easeIn"
+                          >
+                              <div class="text-left">
+                                <h2>IT의 가치 넥스모어가 만들어 갑니다.</h2>
+                              </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="item">
+                        <img src="images/main-slider/2.jpg"   />
+                        <div class="carousel-caption">
+                         <div
+                            class="tp-caption sft sfb tp-resizeme"
+                            data-x="left"
+                            data-hoffset="15"
+                            data-y="center"
+                            data-voffset="-90"
+                            data-speed="1500"
+                            data-start="0"
+                            data-easing="easeOutExpo"
+                            data-splitin="none"
+                            data-splitout="none"
+                            data-elementdelay="0.01"
+                            data-endelementdelay="0.3"
+                            data-endspeed="1200"
+                            data-endeasing="Power4.easeIn"
+                          >
+                              <div class="text-left">
+                                <h2> 끊임없이 변화하는 넥스모어는 더 나은 세상을 만들어 갑니다.</h2>
+                              </div>
+                        </div>
+                      </div>
+                    </div>
+                   
+                    
                 </div>
+                <a class="left carousel-control" href="#categoryExampleSlider" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#categoryExampleSlider" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
 
-                <div
-                  class="tp-caption sft sfb tp-resizeme"
-                  data-x="left"
-                  data-hoffset="15"
-                  data-y="center"
-                  data-voffset="15"
-                  data-speed="1500"
-                  data-start="500"
-                  data-easing="easeOutExpo"
-                  data-splitin="none"
-                  data-splitout="none"
-                  data-elementdelay="0.01"
-                  data-endelementdelay="0.3"
-                  data-endspeed="1200"
-                  data-endeasing="Power4.easeIn"
-                >
-                  <div class="text">
-                    끊임없이 변화하는 넥스모어는 더 나은 세상을 만들어 갑니다.
-                  </div>
-                </div>
-
-                <div
-                  class="tp-caption sfb sfb tp-resizeme"
-                  data-x="left"
-                  data-hoffset="15"
-                  data-y="center"
-                  data-voffset="90"
-                  data-speed="1500"
-                  data-start="1000"
-                  data-easing="easeOutExpo"
-                  data-splitin="none"
-                  data-splitout="none"
-                  data-elementdelay="0.01"
-                  data-endelementdelay="0.3"
-                  data-endspeed="1200"
-                  data-endeasing="Power4.easeIn"
-                >
-                  <a href="nexmore/about.html" class="theme-btn btn-style-one">
-                    Read More
-                  </a>
-                  &ensp;&ensp;
-                  <a
-                    href="service/business.html"
-                    class="theme-btn btn-style-two"
-                  >
-                    NEXMORE SERVICES
-                  </a>
-                </div>
-              </li>
-              <li
-                data-transition="fade"
-                data-slotamount="1"
-                data-masterspeed="1000"
-                data-thumb="images/main-slider/2.jpg"
-                data-saveperformance="off"
-                data-title="Awesome Title Here"
-              >
-                <img
-                  src="images/main-slider/2.jpg"
-                  alt=""
-                  data-bgposition="center top"
-                  data-bgfit="cover"
-                  data-bgrepeat="no-repeat"
-                />
-
-                <div class="slide-overlay"></div>
-
-                <div
-                  class="tp-caption sft sfb tp-resizeme"
-                  data-x="left"
-                  data-hoffset="15"
-                  data-y="center"
-                  data-voffset="-90"
-                  data-speed="1500"
-                  data-start="0"
-                  data-easing="easeOutExpo"
-                  data-splitin="none"
-                  data-splitout="none"
-                  data-elementdelay="0.01"
-                  data-endelementdelay="0.3"
-                  data-endspeed="1200"
-                  data-endeasing="Power4.easeIn"
-                >
-                  <div class="text-left">
-                    <h2>
-                      "가치경영, 고객만족, 행복경영" 최고의 IT기업을 추구합니다.
-                    </h2>
-                  </div>
-                </div>
-
-                <div
-                  class="tp-caption sft sfb tp-resizeme"
-                  data-x="left"
-                  data-hoffset="15"
-                  data-y="center"
-                  data-voffset="15"
-                  data-speed="1500"
-                  data-start="500"
-                  data-easing="easeOutExpo"
-                  data-splitin="none"
-                  data-splitout="none"
-                  data-elementdelay="0.01"
-                  data-endelementdelay="0.3"
-                  data-endspeed="1200"
-                  data-endeasing="Power4.easeIn"
-                >
-                  <div class="text">
-                    고객의 다양한 Needs와 기술력과 특화된 솔루션으로 고객만족에
-                    힘쓰겠습니다.
-                  </div>
-                </div>
-
-                <div
-                  class="tp-caption sfb sfb tp-resizeme"
-                  data-x="left"
-                  data-hoffset="15"
-                  data-y="center"
-                  data-voffset="90"
-                  data-speed="1500"
-                  data-start="1000"
-                  data-easing="easeOutExpo"
-                  data-splitin="none"
-                  data-splitout="none"
-                  data-elementdelay="0.01"
-                  data-endelementdelay="0.3"
-                  data-endspeed="1200"
-                  data-endeasing="Power4.easeIn"
-                >
-                  <a href="nexmore/about.html" class="theme-btn btn-style-one">
-                    Read More
-                  </a>{" "}
-                  &ensp;&ensp;
-                  <a
-                    href="infoComm/business.html"
-                    class="theme-btn btn-style-two"
-                  >
-                    NEXMORE SERVICES
-                  </a>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
       </section>
+
+
+
       {/* defaultsection1*/}
       <section className="default-section">
         <div className="auto-container">
