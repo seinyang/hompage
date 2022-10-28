293940c22503a678d51ca8c4bd2cf99a74a41c06
세인 양 <did756984@naver.com>
2022-10-28 18:17:55 +0900

    dd

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ function App() {
         <Route path="/map" element={<Map />} />
       </Routes>
 
-      {/* 항상 보여야하는 footer */}
+      {/* 항상 보여야하는 footerd d */}
       <Footer />
     </Fragment>
   );
